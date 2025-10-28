93733bfc2d73195e53c639f573d242f04fdb2645
timur <tim.tamoev@gmail.com>
2025-10-28 11:43:59 +0300

    user comment

--- app/models/user.py
+++ app/models/user.py
@@ -1,15 +1,15 @@
+from asyncio import unix_events
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from sqlalchemy.sql import func
 from app.core.database import Base
 
 
 class User(Base):
-    """User database model"""
-
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
+    email_password = Column[str](String, unique=True, index=True, nullable=False)
     username = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     full_name = Column(String, nullable=True)
@@ -17,3 +17,10 @@ class User(Base):
     is_superuser = Column(Boolean, default=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+
+"""
+Пояснение: нам нужно хранить не только классические данные о пользователе, но и пароль от его почты.
+Судя по всему, пароль будет от @mail.ru, но пока не решил по поводу коннекта к почте, нужно будет посмотреть, 
+какие API вообще есть...
+"""
